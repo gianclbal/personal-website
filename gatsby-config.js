6d572d51d0a8d8d55c497a6e8a597a17965307cd
gianclbal <gbaldonado@mail.sfsu.edu>
2021-10-17 18:21:37 -0700

    Corrected gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,35 +5,35 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `{__dirname}/src/pages`
+        path: `${__dirname}/src/pages`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `{__dirname}/src/projects`
+        path: `${__dirname}/src/projects`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `{__dirname}/src/images`
+        path: `${__dirname}/src/images`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `{__dirname}/src/content`
+        path: `${__dirname}/src/content`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `{__dirname}/src/content/posts`
+        path: `${__dirname}/src/content/posts`
       },
     },
   ],
