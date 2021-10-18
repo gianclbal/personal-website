be699636ac3d960650c7f7f46c232b32a40ce7c5
gianclbal <gbaldonado@mail.sfsu.edu>
2021-10-17 18:19:35 -0700

    Added gatsby-source-file-system

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `{__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `{__dirname}/src/projects`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `{__dirname}/src/images`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `{__dirname}/src/content`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `{__dirname}/src/content/posts`
+      },
+    },
+  ],
 }
