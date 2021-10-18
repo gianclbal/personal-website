c260a77db5d8079f481ef2a3be7009333967aac7
gianclbal <gbaldonado@mail.sfsu.edu>
2021-10-18 11:36:16 -0700

    Added themes

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,9 +1,29 @@
 export default {
     fonts: {
         main: "Inter, sans-serif",
-        secondary: "Roboto Mono, monospace",
+        code: "Roboto Mono, monospace",
     },
     colors: {
-        
-    }
+        primary: "hsl(208,39%,32%)",
+        accent: "hsl(180,45%,54%)",
+        white: "hsl(240,11%,96%)",
+        black: "hsl(240,3%,12%)",
+    },
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem",
+    },
+    spacings: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+        xLarge: "4rem",
+        xxLarge: "6rem",
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease",
+    }, 
 }
