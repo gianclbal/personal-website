6c709adb3636144b0d1c86848cd4a6f0044bfd17
gianclbal <gbaldonado@mail.sfsu.edu>
2021-10-22 19:23:32 -0700

    Nav updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, Link, graphql } from "gatsby"
-import { NavWrapper, StyledNav } from "../elements"
+import { NavWrapper, StyledNav, StyledNavLinks } from "../elements"
 
 export const Nav = () => {
   const data = useStaticQuery(graphql`
@@ -11,12 +11,44 @@ export const Nav = () => {
     }
   `)
 
+  const navLinks = [
+    {
+      name: "About",
+      url: "/#about",
+    },
+    {
+      name: "Work",
+      url: "/#projects",
+    },
+    {
+      name: "Experience",
+      url: "/#jobs",
+    },
+    {
+      name: "Contact",
+      url: "/#contact",
+    },
+    {
+      name: "Learn More",
+      url: "/#learn",
+    },
+  ]
+
   return (
     <NavWrapper>
       <StyledNav>
         <Link to="/">
           <img src={data.logo.publicURL} alt="My Logo" />
         </Link>
+        <StyledNavLinks>
+          <ol>
+            {navLinks.map(({ url, name }, i) => (
+              <li key={i}>
+                <Link to={url}>{name}</Link>
+              </li>
+            ))}
+          </ol>
+        </StyledNavLinks>
       </StyledNav>
     </NavWrapper>
   )
--- src/elements/NavElements.js
+++ src/elements/NavElements.js
@@ -2,11 +2,12 @@ import styled from "styled-components"
 
 export const NavWrapper = styled.header`
   display: flex;
-  margin: 0 34px;
-  padding: 0 50px;
-  display: flex;
+  justify-content: space-between;
   align-items: center;
-  justify-content: flex-start;
+  position: fixed;
+  /* margin: 0 34px; */
+  z-index: 11;
+  padding: 0 50px;
   width: 100%;
   height: 100px;
   backdrop-filter: blur(10px);
@@ -15,38 +16,47 @@ export const NavWrapper = styled.header`
     padding: 0 40px;
   }
   @media ${props => props.theme.breakpoints.mobile} {
-    padding: 0 0px;
+    padding: 0 25px;
   }
 
+  .scrolling-active{
+    background-color: ${props => props.theme.colors.primary};
+    
+  }
 `
 
 export const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   position: relative;
+  z-index: 12;
   width: 100%;
   color: ${props => props.theme.colors.primary};
   img {
     height: 45px;
     transition: filter 0.3s ease;
-    
   }
 
-  & img:hover, 
-        img:focus {
-          filter: invert(30%) sepia(9%) saturate(2620%) hue-rotate(166deg) brightness(93%) contrast(86%);
-    }
-
-
-` 
+  & img:hover,
+  img:focus {
+    filter: invert(30%) sepia(9%) saturate(2620%) hue-rotate(166deg)
+      brightness(93%) contrast(86%);
+  }
+`
 
 export const StyledNavLinks = styled.div`
   display: flex;
   align-items: center;
 
-  @media ${props => props.theme.breakpoints.tablet}{
+  @media ${props => props.theme.breakpoints.tablet} {
     display: none;
   }
 
   ol {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     padding: 0;
     margin: 0;
     list-style: none;
@@ -55,15 +65,9 @@ export const StyledNavLinks = styled.div`
       margin: 0 5px;
       position: relative;
 
-      @ {
+      a {
         padding: 10px;
-
-        
       }
     }
-
-
   }
-
-
 `
